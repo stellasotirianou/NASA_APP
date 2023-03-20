7c8d829390d7cb09dde7f68ef855450fa564d604
stellasotirianou <stella.sotirianou@gmail.com>
2023-03-20 08:10:20 +0200

    feat: date picker

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
 import React, { useEffect, useState } from "react";
-import ImageCard from "./ImageCard";
-import dayjs, { Dayjs } from "dayjs";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { Grid } from "@mui/material";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+//import DatePickerValue from "react-datepicker"; //from "./DatePicker"
+//import ImageCard from "./ImageCard";
+
 
 function App() {
-  const [data, setData] = useState([]);
-  const [value, setValue] = useState(dayjs("2022-04-17"));
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      "https://api.nasa.gov/planetary/apod?api_key=mY5U7bQe6xACrnedLZj9il1o918YR0xNuk6S1F45"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch(
+  //     "https://api.nasa.gov/planetary/apod?api_key=mY5U7bQe6xACrnedLZj9il1o918YR0xNuk6S1F45"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Grid container rowSpacing={8}>
-        <Grid item>
-          <DatePicker
-            value={value}
-            onChange={(newValue) => setValue(newValue)}
-          />
-        </Grid>
-        <Grid item>
-          <ImageCard data={data} />
-        </Grid>
-      </Grid>
-    </LocalizationProvider>
+  // return (
+  //   <DatePickerValue />
+  // );
+
+  // DOULEUEI
+ 
+  // const [date, setDate] = useState();
+  // console.log("Date", date);
+  // return(
+  // <>
+  // <h1>Select Date: {date}</h1>
+  // <input type="date"  placeholder="dd-mm-yyyy" onChange={e=>setDate(e.target.value)}/>
+
+  // </>
+  // );
+
+  const [selectedDate, setSelectedDate] = useState(null);
+  return(
+    <div>
+      <DatePicker selected={selectedDate} onChange={date => setSelectedDate(date)} />
+    </div>
   );
 }
 
