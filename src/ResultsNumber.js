654824a32be47f4c499e2bfa135fabffcabe0c03
stellasotirianou <stella.sotirianou@gmail.com>
2023-04-29 16:29:56 +0300

    new component for results number

---
+++ src/ResultsNumber.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Typography from "@mui/material/Typography";
+import "./App.css";
+const ResultsNumber = ({ text }) => {
+  return (
+    <Row>
+      <Col>
+        <Typography
+          variant="subtitle"
+          align="center"
+          component="div"
+          className="heading"
+          style={{ margin: "20px", color: "#43484c" }}
+        >
+          {text} events found for the past 10 days
+        </Typography>
+      </Col>
+    </Row>
+  );
+};
+
+export default ResultsNumber;
