f9139e972ebf53c0d2427574a256c6a2eed990a1
stellasotirianou <stella.sotirianou@gmail.com>
2023-04-30 14:55:20 +0300

    added sub header option

--- src/Header.js
+++ src/Header.js
@@ -3,21 +3,42 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Typography from "@mui/material/Typography";
 import "./App.css";
-const Header = ({ headerText }) => {
+const Header = ({ headerText, subHeader }) => {
   return (
-    <Row>
-      <Col style={{ backgroundColor: "#f9f9f9" }}>
-        <Typography
-          variant="h5"
-          align="center"
-          component="div"
-          className="heading"
-          style={{ margin: "20px", color: "#43484c" }}
-        >
-          {headerText}
-        </Typography>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col style={{ backgroundColor: "#f9f9f9" }}>
+          <Typography
+            variant="h5"
+            align="center"
+            component="div"
+            className="heading"
+            style={{
+              marginTop: "20px",
+              marginBottom: "10px",
+              color: "#43484c",
+            }}
+          >
+            {headerText}
+          </Typography>
+        </Col>
+      </Row>
+      {subHeader && (
+        <Row>
+          <Col style={{ backgroundColor: "#f9f9f9" }}>
+            <Typography
+              variant="subtitle"
+              align="center"
+              component="div"
+              className="heading"
+              style={{ marginBottom: "20px", color: "#43484c" }}
+            >
+              {subHeader}
+            </Typography>
+          </Col>
+        </Row>
+      )}
+    </>
   );
 };
 
